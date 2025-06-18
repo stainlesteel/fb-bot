0cb06f67ca31a904388f2ccb0ad197444d3818e2
stainlesteel <149646073+stainlesteel@users.noreply.github.com>
2025-06-17 19:04:40 -0600

    add files via upload

---
+++ bots.py
@@ -0,0 +1,29 @@
+import os
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+from discord.commands import SlashCommandGroup, Option
+
+
+tool = ('get your own')
+
+bot = commands.Bot()
+
+
+@bot.slash_command(
+    name="fb",
+    description="sends fb."
+)
+
+async def fb(ctx: discord.ApplicationContext, number: int = Option(int, description="Enter number of output times.", min_value=1)):
+     outppot = "fb\n" * number
+     await ctx.respond(outppot) 
+
+  
+
+bot.run(tool)
+
+
+
+
+ 
